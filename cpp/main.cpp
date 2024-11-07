aa17c63ccbe053d4128bad029295805b459ba2a9
Shin Umeda <umeda.shin@gmail.com>
2024-11-06 22:18:07 -0800

    moved to a type-value system, implemented splitWhen to replace findSplit

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -11,8 +11,8 @@ template <char c, char... cs> struct head {
 template <char... cs> struct tstring;
 template <> struct tstring<> {};
 template <char c, char... cs> struct tstring<c, cs...> {
-  static constexpr char getHead() noexcept { return c; }
-  static constexpr auto tail() noexcept { return tstring<cs...>{}; }
+  using head = tstring<c>;
+  using tail = tstring<cs...>;
 };
 
 template <char c> void serialize_(std::string &str, tstring<c>) {
@@ -51,25 +51,24 @@ template <typename a, typename b> struct pair {
   using B = b;
 };
 
-template <auto, typename, typename> struct delimitWhen_;
+template <template <char> typename, typename, typename> struct delimitWhen_;
 
-template <auto p, char... cs>
+template <template <char> typename p, char... cs>
 struct delimitWhen_<p, tstring<>, tstring<cs...>>
     : returns<pair<tstring<cs...>, tstring<>>> {};
 
-template <auto pred, char... in, char... out>
+template <template <char> typename pred, char... in, char... out>
 struct delimitWhen_<pred, tstring<in...>, tstring<out...>>
     : returns<typename if_else<
-          pred(head<in...>::value),
-          pair<tstring<out...>, decltype(tstring<in...>::tail())>,
+          pred<head<in...>::value>::value,
+          pair<tstring<out...>, typename tstring<in...>::tail>,
           typename delimitWhen_<
-              pred, decltype(tstring<in...>::tail()),
+              pred, typename tstring<in...>::tail,
               typename append<tstring<out...>, tstring<head<in...>::value>>::
                   type>::type>::type> {};
 
-template <auto pred, typename in>
-struct delimitWhen : returns<typename delimitWhen_<pred, in, tstring<>>::type> {
-};
+template <template <char> typename pred, typename in>
+using delimitWhen = typename delimitWhen_<pred, in, tstring<>>::type;
 
 struct Whitespace {};
 struct Numeric {};
@@ -77,7 +76,7 @@ struct Paren {};
 struct Operator {};
 struct Other {};
 
-template <char c> struct classify;
+template <char> struct classify;
 
 constexpr bool isWhitespace(char c) {
   return c == ' ' or c == '\t' or c == '\n';
@@ -110,12 +109,43 @@ struct classify<c> : returns<Paren> {};
 template <char c>
   requires(isOp(c))
 struct classify<c> : returns<Operator> {};
-template <char c> struct classify : returns<Other> {};
+template <char> struct classify : returns<Other> {};
+
+template <template <char, char> typename, typename, typename> struct splitWhen_;
+
+template <template <char, char> typename p, char... cs>
+struct splitWhen_<p, tstring<>, tstring<cs...>>
+    : returns<pair<tstring<cs...>, tstring<>>> {};
+
+template <template <char, char> typename pred, char... in, char... out>
+struct splitWhen_<pred, tstring<in...>, tstring<out...>>
+    : returns<typename if_else<
+          pred<head<in...>::value, head<out...>::value>::value,
+          pair<tstring<out...>, tstring<in...>>,
+          typename splitWhen_<
+              pred, typename tstring<in...>::tail,
+              typename append<tstring<out...>, tstring<head<in...>::value>>::
+                  type>::type>::type> {};
+
+template <template <char, char> typename pred, typename in>
+using splitWhen =
+    typename splitWhen_<pred, typename in::tail, typename in::head>::type;
+
+template <char c> struct is_space {
+  constexpr static bool value = c == ' ';
+};
+
+template <char a, char b> struct split_types {
+  constexpr static bool value =
+      !std::is_same_v<typename classify<a>::type, typename classify<b>::type>;
+};
 
 using hello_world =
     tstring<'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'>;
-using n = delimitWhen<[](char c) { return c == ' '; }, hello_world>::type;
+using n = delimitWhen<is_space, hello_world>;
 using a = n::A;
+
+using m = splitWhen<split_types, hello_world>;
 
 int main() {
   auto tstr = hello_world{};
